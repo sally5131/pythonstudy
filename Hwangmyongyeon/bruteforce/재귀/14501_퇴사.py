b50f166b930de0d057eea08a2dd45d00151cbd24
auddus16 <auddus16@naver.com>
2021-07-22 23:34:21 +0900

    auddus21.07.23

--- Hwangmyongyeon/bruteforce/재귀/14501_퇴사.py
+++ Hwangmyongyeon/bruteforce/재귀/14501_퇴사.py
@@ -5,13 +5,16 @@ for _ in range(N):
     t, p= map(int, sys.stdin.readline().split())
     T.append(t)
     P.append(p)
-a=[]
-def go(i, res):
-    if i>7:
-        a.append(res)
+res=0
+def go(i, total):
+    global res
+    if i == N:
+        res= max(total, res)
         return
-    go(i+T[i], res+P[i])
-    go(i+1, res)
+    if i > N:
+        return
+    go(i+1, total)
+    go(i+T[i], total+P[i])
 
 go(0, 0)
-print(max(a))
+print(res)
