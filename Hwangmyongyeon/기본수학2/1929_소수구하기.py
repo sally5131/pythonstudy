9e76196b4cc2f3abbffab2c80bf4211016799e59
auddus16 <auddus16@naver.com>
2021-08-04 00:42:56 +0900

    my210804

--- Hwangmyongyeon/기본수학2/1929_소수구하기.py
+++ Hwangmyongyeon/기본수학2/1929_소수구하기.py
@@ -1,2 +1,15 @@
 import sys
-N, M= map(int, sys.stdin.readline().split())
+M, N= map(int, sys.stdin.readline().split())
+check=[False]*(N+1)
+prime=list()
+def isPrime(n):
+    for i in range(2, n+1):
+        if not check[i]:
+           prime.append(i)
+        for k in range(i*2, n+1, i):
+            check[k]=True
+isPrime(N)
+for i in prime:
+    if i<M:
+        continue
+    print(i)
