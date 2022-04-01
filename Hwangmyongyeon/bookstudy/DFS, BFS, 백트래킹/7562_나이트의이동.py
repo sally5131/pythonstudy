5ae88025523bd888af2afe7ceda41be27b4df45d
auddus16 <auddus16@naver.com>
2022-04-01 23:06:49 +0900

    명연-220401

--- Hwangmyongyeon/bookstudy/DFS, BFS, 백트래킹/7562_나이트의이동.py
+++ Hwangmyongyeon/bookstudy/DFS, BFS, 백트래킹/7562_나이트의이동.py
@@ -1,21 +1,17 @@
 import sys
-import math
 from collections import deque
 T = int(sys.stdin.readline())
 
-dx = (-2, -2, -1, -1, +1, +1, +2, +2)
-dy = (-2, +2, -1, +1, -2, +2, -1, +2)
+dx = (-1, -1, -2, -2, +1, +1, +2, +2)
+dy = (-2, +2, -1, +1, -2, +2, -1, +1)
 
 for _ in range(T):
     I = int(sys.stdin.readline())
 
-    board = list([False] * I for _ in range(I))
     chk = list([False] * I for _ in range(I))
 
     sr, sc = map(int, sys.stdin.readline().split())
     dr, dc = map(int, sys.stdin.readline().split())
-
-    board[dr][dc] = True
 
     dq = deque()
     dq.append((sr, sc, 0))
@@ -23,7 +19,7 @@ for _ in range(T):
 
     while dq:
         r, c, cnt = dq.popleft()
-        if board[r][c]:
+        if r == dr and c == dc:
             print(cnt)
             break
         for i in range(8):
---
+++ Hwangmyongyeon/bookstudy/DFS, BFS, 백트래킹/9663_N-Queen.py
@@ -0,0 +1,3 @@
+import sys
+N = int(sys.stdin.readline())
+chk = [[False]*N for _ in range(N)]
