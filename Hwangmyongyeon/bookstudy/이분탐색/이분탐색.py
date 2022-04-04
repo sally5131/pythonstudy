0ba1b6c7b089162373d67160d3a45db6481ab096
auddus16 <auddus16@naver.com>
2022-04-04 23:56:20 +0900

    명연-220404

--- Hwangmyongyeon/bookstudy/이분탐색/이분탐색.py
+++ Hwangmyongyeon/bookstudy/이분탐색/이분탐색.py
@@ -1,6 +1,6 @@
 from bisect import bisect_left, bisect_right
 a = [2, 3, 6, 6, 6, 10, 12, 15] # 총 8개 -> 기준은 4번째 값 -> mid = a[3]
-l = bisect_left(a, 5) # a에서 목표값 6보다 같거나 큰 첫번째 값의 위치(index+1)를 반환 index를 구하려면 -1 해야함.
-r = bisect_right(a, 5) # a에서 목표값 6보다 큰 첫번째 값의 위치(index+1)를 반환
+l = bisect_left(a, 5) # a에서 목표값 6보다 같거나 큰 첫번째 값의 위치를 반환
+r = bisect_right(a, 5) # a에서 목표값 6보다 큰 첫번째 값의 위치를 반환
 print(l)
 print(r)
